5232e3e17d2f150fb3045cf7e926ad3c717a421e
Jihan <jihankoloth.gmail.com>
2026-09-14 12:19:27 +0530

    4 commit

--- day1.dart
+++ day1.dart
@@ -1,5 +1,26 @@
 void main(){
-    print("hello");
+ q1();
+ q2();
 
-    
+
 }
+
+ q1(){
+    String? name=null;
+    print(name);
+    name="jihan";
+    print(name);
+ }
+ q2(){
+   String? text;
+   text="jihan";
+   text=null;
+   text="jihan";
+   text=null;
+   print(text);
+   text="jinu";
+   print(text);
+   print(text.length);
+   text=null;
+   print(text);
+ }
