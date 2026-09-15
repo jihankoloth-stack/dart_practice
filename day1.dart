30eaef6ed82215e13860ee241825f89d2b2e235f
Jihan <jihankoloth.gmail.com>
2026-09-15 09:48:56 +0530

    5commit

--- day1.dart
+++ day1.dart
@@ -1,26 +1,68 @@
 void main(){
- q1();
- q2();
+//  q1();
+//  q2();
+    q3();
+    q4();
+}
 
+//  q1(){
+//     String? name=null;
+//     print(name);
+//     name="jihan";
+//     print(name);
+//  }
+//  q2(){
+//    String? text;
+//    text="jihan";
+//    text=null;
+//    text="jihan";
+//    text=null;
+//    print(text);
+//    text="jinu";
+//    print(text);
+//    print(text.length);
+//    text=null;
+//    print(text);
+//  }
+
+q3(){
+
+String? name="jihan";
+print(name!.length);
+name="jinu";
+print(name!.length);
+
+
+int? number=3746542;
+print(number!.toString().length);
+number=5674;
+print(number!.toString().length);
+print(number!+226);
+print(number!+326);
+print(number!.isEven);
+print(number!.isOdd);
 
 }
 
- q1(){
-    String? name=null;
-    print(name);
+q4(){
+    String?name=null;
+    print(name??"ronaldoo");
     name="jihan";
-    print(name);
- }
- q2(){
-   String? text;
-   text="jihan";
-   text=null;
-   text="jihan";
-   text=null;
-   print(text);
-   text="jinu";
-   print(text);
-   print(text.length);
-   text=null;
-   print(text);
- }
+    print(name??"jinu");
+    int ? number=null;
+    print(number?? 65);
+    print(number?? 19);
+    String? city=null;
+    print(city?? "kochi");
+    String? names=null;
+    String? username="jihan";
+    print(names?? "muhammed jihan");
+    print(username);
+    String?gmail=null;
+    print("gmail: ${gmail?? "jihan@gmsil.com"}");
+}
+
+
+
+
+
