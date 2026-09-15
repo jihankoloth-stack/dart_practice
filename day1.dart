8da063d0b3c5bebd8425de68e5223e011b655424
Jihan <jihankoloth.gmail.com>
2026-09-15 10:39:32 +0530

    6commit

--- day1.dart
+++ day1.dart
@@ -1,8 +1,9 @@
 void main(){
 //  q1();
 //  q2();
-    q3();
-    q4();
+    // q3();
+    // q4();
+    q5();
 }
 
 //  q1(){
@@ -25,43 +26,65 @@ void main(){
 //    print(text);
 //  }
 
-q3(){
+// q3(){
 
-String? name="jihan";
-print(name!.length);
-name="jinu";
-print(name!.length);
+// String? name="jihan";
+// print(name!.length);
+// name="jinu";
+// print(name!.length);
 
 
-int? number=3746542;
-print(number!.toString().length);
-number=5674;
-print(number!.toString().length);
-print(number!+226);
-print(number!+326);
-print(number!.isEven);
-print(number!.isOdd);
+// int? number=3746542;
+// print(number!.toString().length);
+// number=5674;
+// print(number!.toString().length);
+// print(number!+226);
+// print(number!+326);
+// print(number!.isEven);
+// print(number!.isOdd);
+
+// }
+
+// q4(){
+//     String?name=null;
+//     print(name??"ronaldoo");
+//     name="jihan";
+//     print(name??"jinu");
+//     int ? number=null;
+//     print(number?? 65);
+//     print(number?? 19);
+//     String? city=null;
+//     print(city?? "kochi");
+//     String? names=null;
+//     String? username="jihan";
+//     print(names?? "muhammed jihan");
+//     print(username);
+//     String?gmail=null;
+//     print("gmail: ${gmail?? "jihan@gmsil.com"}");
+// }
+q5(){
+    late String name;
+    name ="jihan";
+    print(name);
+
+    String?text;
+    text=null;
+    print(text);
+
+    late String firstname;
+    String?lastname;
+
+    firstname="muhammed";
+    lastname="jihan";
+    print(firstname);
+    print(lastname);
+
+
+
+
+
 
 }
-
-q4(){
-    String?name=null;
-    print(name??"ronaldoo");
-    name="jihan";
-    print(name??"jinu");
-    int ? number=null;
-    print(number?? 65);
-    print(number?? 19);
-    String? city=null;
-    print(city?? "kochi");
-    String? names=null;
-    String? username="jihan";
-    print(names?? "muhammed jihan");
-    print(username);
-    String?gmail=null;
-    print("gmail: ${gmail?? "jihan@gmsil.com"}");
-}
-
 
 
 
